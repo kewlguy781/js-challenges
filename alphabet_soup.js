285da0626cc290fe742e1933a2d97532d080baad
Jon Roberts <kewlguy@gmail.com>
2020-09-09 10:40:15 -0600

    alphabet soup challenge

---
+++ alphabet_soup.js
@@ -0,0 +1,21 @@
+//Have the function alphabetSoup(str) take the str string 
+// parameter being passed and return the string with the 
+// letters in alphabetical order (ie. hello becomes ehllo). 
+// Assume numbers and punctuation symbols will not be 
+// included in the string. 
+
+let alphabetSoup = 'hello'
+
+function (alphabetSoup) {
+
+// copy the string and break into characters array
+let stringA= alphabetSoup.split("")
+stringA.sort()
+return stringA
+
+
+
+//return
+
+
+}
